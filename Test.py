98f6f253c9b88df20da1712b5f28606f7bc6349a
CretuBogdanDaniel <92577924+CretuBogdanDaniel@users.noreply.github.com>
2022-04-04 13:35:10 +0300

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print("Domo")
+lista = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+lista_ascendeta = sorted(lista)
+lista_descendenta = sorted(lista,reverse=True)
+def mul3():
+    for i in lista:
+        if(i % 3 == 0):
+            print(i, end = ", ")
+
+print(lista_descendenta)
+print(lista_ascendeta)
+print("Multipli lui 3 sunt: ")
+print(end = mul3())
+
